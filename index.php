f6c25fc5eeb84b1cd847dc60be293e58bc8a7754
rachanasb23 <94848409+rachanasb23@users.noreply.github.com>
2022-03-11 19:09:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <body>
 <h1>Welcome to my web app!</h1>
 <?php
-echo "<h2>This web app is created using Ms Azure App Services</h2>
+echo "<h2>This web app is created using Ms Azure App Services</h2>";
 ?>
 <h3>Cloud Computing Track from Edunet!</h3>
 <?php
-echo "<h4>Thank you for visiting</h4>
+echo "<h2>Thank you for visiting Us!!</h2>";
 ?>
 </body>
 </html>
