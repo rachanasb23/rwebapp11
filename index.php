95d32f318cbcdd95a5d87d6cf56ae6679d0dc15e
rachanasb23 <94848409+rachanasb23@users.noreply.github.com>
2022-03-11 18:58:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>My Web App!!</title>
+</head>
+<body>
+<h1>Welcome to my web app!</h1>
+<?php
+echo "<h2>This web app is created using Ms Azure App Services</h2>
+?>
+<h3>Cloud Computing Track from Edunet!</h3>
+<?php
+echo "<h4>Thank you for visiting</h4>
+?>
+</body>
+</html>
+
